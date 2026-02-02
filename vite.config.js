ae960b68014a2f9da12d1792bdfdb91e61bb31c7
Misikerr <mgtube75@gmail.com>
2026-02-02 08:46:00 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, 
+    allowedHosts: [
+      '.ngrok-free.app',
+      '1a9abfafd456.ngrok-free.app'
+    ],
+  },
+})
